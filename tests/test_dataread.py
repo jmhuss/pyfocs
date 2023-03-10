77909934e65d729106f01719e08c7140ada36e10
klapo <lapo.karl@gmail.com>
2023-03-10 11:36:20 +0100

    Formatting: Enforced black format for tests

--- tests/test_dataread.py
+++ tests/test_dataread.py
@@ -1,25 +1,24 @@
 import os
-# import pyfocs
 from pyfocs import xml_read
 
 
 # Paths to example data
 path = os.path.dirname(os.path.abspath(__file__))
-path_data = os.path.join(path, 'data')
-mc_demo = 'multifiledemo'
-sf_demo = 'singlefiledemo'
-channel = 'channel 1'
-sfn = 'channel 1_20190722000003996.xml'
+path_data = os.path.join(path, "data")
+mc_demo = "multifiledemo"
+sf_demo = "singlefiledemo"
+channel = "channel 1"
+sfn = "channel 1_20190722000003996.xml"
 
-path_data_singlefile = os.path.join(path_data, sf_demo, 'raw_xml', channel, sfn)
+path_data_singlefile = os.path.join(path_data, sf_demo, "raw_xml", channel, sfn)
 path_data_multifile = os.path.join(path_data, mc_demo)
-yaml_file = os.path.join(path_data, 'example_configuration.yml')
+yaml_file = os.path.join(path_data, "example_configuration.yml")
 
 
 def test_example_data_exists():
-    '''
+    """
     Test that the example data is found along the hard coded paths.
-    '''
+    """
     # The multicore data directory exists.
     assert os.path.isdir(path_data_multifile)
 
@@ -29,8 +28,8 @@ def test_example_data_exists():
 
 
 def test_xmlread():
-    '''
+    """
     Test the ability to read a single xml file and return the expected data.
-    '''
+    """
     # Did we read the file without getting a CorruptedXMLError exception?
     assert xml_read(path_data_singlefile)
